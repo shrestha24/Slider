1356ad4b6afced43e844a2ff4df2ef0eca97ffb5
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2020-08-16 01:25:25 +0530

    Slider

---
+++ app/src/main/java/com/example/addslider/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.addslider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+    ViewFlipper v_flipper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int images[] = {R.drawable.mainpagedoctor1, R.drawable.banner6, R.drawable.banner1medical11, R.drawable.banner7, R.drawable.banner5medical1};
+
+        v_flipper = findViewById(R.id.v_flipper);
+
+        //for loop
+        for (int image: images) {
+            flipperImages(image);
+
+        }
+    }
+
+    public void flipperImages(int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(2000);
+        v_flipper.setAutoStart(true);
+
+        //animation.
+
+        v_flipper.setInAnimation(this, android.R.anim.slide_in_left);
+        v_flipper.setOutAnimation(this, android.R.anim.slide_out_right);
+    }
+    }
